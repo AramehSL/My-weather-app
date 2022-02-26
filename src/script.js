c17b11833ec8d53a9eaf7994ca1f4fc52a3cb1f1
Arameh Soltani <aramehsl@gmail.com>
2022-02-26 11:00:17 -0500

    adding Api

--- src/script.js
+++ src/script.js
@@ -71,7 +71,8 @@ formSearchBtn.addEventListener("click", search);
 
 //////       Forecast       /////////////////////////////
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -102,6 +103,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+//////        Coords Function /////////////////////////////
+
+function getForecast(coordinates) {
+  let apiKey = "88ce603b3361b10f232b41d49c2e52e3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //////        Location Button /////////////////////////////
 
 function showCurrentTempreture(response) {
@@ -121,6 +130,8 @@ function showCurrentTempreture(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function getPosition(position) {
